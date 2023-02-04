bea5650540e68cf20d51ce5e633cb579fb9059cd
ÇETİN <cetinnnarasss@gmail.com>
2023-02-04 11:28:54 +0300

    approuter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css"
 import Navbar from "./components/Navbar"
+import AuthContext from "./contexts/AuthContext"
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <AuthContext>
+        <Navbar />
+      </AuthContext>
+      
     </div>
   )
 }

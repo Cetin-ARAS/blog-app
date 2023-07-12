579c68d570d4c2c69aad001ee240286964f3d396
ÇETİN <cetinnnarasss@gmail.com>
2023-07-13 01:45:04 +0300

    firebase update

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -2,21 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const app = initializeApp({
-  apiKey: "AIzaSyDmFVRUF7SNy_LtOZm0MU7l4O0vGt4htPM",
-  authDomain: "blogg-c6b29.firebaseapp.com",
-  databaseURL: "https://blogg-c6b29-default-rtdb.firebaseio.com/",
-  projectId: "blogg-c6b29",
-  storageBucket: "blogg-c6b29.appspot.com",
-  messagingSenderId: "601515994240",
-  appId: "1:601515994240:web:ced870f60b050ba0240a65",
-
-  // REACT_APP_FIREBASE_apiKey=AIzaSyDmFVRUF7SNy_LtOZm0MU7l4O0vGt4htPM
-  // REACT_APP_FIREBASE_authDomain=blogg-c6b29.firebaseapp.com
-  // REACT_APP_FIREBASE_projectId=blogg-c6b29
-  // REACT_APP_FIREBASE_databaseURL=https://blogg-c6b29-default-rtdb.firebaseio.com/
-  // REACT_APP_FIREBASE_storageBucket=blogg-c6b29.appspot.com
-  // REACT_APP_FIREBASE_messagingSenderId=601515994240
-  // REACT_APP_FIREBASE_appId=1:601515994240:web:ced870f60b050ba0240a65
+  apiKey: "AIzaSyC4pSeihkQf9ZB1C3LqsgWroIMiW4EEvWg",
+  authDomain: "blog-app-d4055.firebaseapp.com",
+  databaseURL: "https://blog-app-d4055-default-rtdb.firebaseio.com",
+  projectId: "blog-app-d4055",
+  storageBucket: "blog-app-d4055.appspot.com",
+  messagingSenderId: "152652751534",
+  appId: "1:152652751534:web:6fa315ea62d876c93e833f",
+  measurementId: "G-H2N43QPY18",
 });
 
 export const auth = getAuth(app);

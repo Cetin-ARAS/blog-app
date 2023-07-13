3721889e77b53c3161860da8680a168160e1cc99
ÇETİN <cetinnnarasss@gmail.com>
2023-07-13 03:47:18 +0300

    .

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const app = initializeApp({
-  apiKey: "AIzaSyC4pSeihkQf9ZB1C3LqsgWroIMiW4EEvWg",
-  authDomain: "blog-app-d4055.firebaseapp.com",
-  databaseURL: "https://blog-app-d4055-default-rtdb.firebaseio.com",
-  projectId: "blog-app-d4055",
-  storageBucket: "blog-app-d4055.appspot.com",
-  messagingSenderId: "152652751534",
-  appId: "1:152652751534:web:6fa315ea62d876c93e833f",
-  measurementId: "G-H2N43QPY18",
+  apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+  authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+  databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
+  projectId: process.env.REACT_APP_FIREBASE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_appId,
+  measurementId: process.env.REACT_APP_FIREBASE_measurementId,
 });
 
 export const auth = getAuth(app);
